6540d77a73c5bc0a17215ea38be25f5ff7251e5f
Mscuaz <proskidded@gmail.com>
2025-08-11 10:53:05 +0800

    Update Diamond.lua

--- Diamond.lua
+++ Diamond.lua
@@ -6,7 +6,7 @@ if getgenv().RunScript == true then	return end
 getgenv().RunScript = true
 
 local queueScript = string.format([[
-loadstring(game:HttpGet(''))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Mscuaz/Test/refs/heads/main/Diamond.lua'))()
 ]])
 
 queue_on_teleport(queueScript)
